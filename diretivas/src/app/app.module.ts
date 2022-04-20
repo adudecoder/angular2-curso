17ad3212b084922899cc89fece92c84aaa75a9b2
Victor Martins <vitorchiesa74@gmail.com>
2022-04-20 15:03:43 -0300

    Angular CLI: Criando components, services: ng generate

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent
   ],
   imports: [
     BrowserModule
